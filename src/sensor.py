05662c48cc96599827cb85b7ba06857bc5e30f66
woodse07 <seamuswoods10@gmail.com>
2020-12-10 19:52:00 +0000

    sensor.py takes in a port number for the selected sensor to broadcast to

--- src/sensor.py
+++ src/sensor.py
@@ -2,6 +2,8 @@ import argparse
 import random
 import time
 import json
+import flask
+import threading
 from opensimplex import OpenSimplex
 
 
@@ -13,6 +15,10 @@ def construct_packet(keys, values):
 
 
 def simulate_speed():
+    global INITIAL_SPEED
+    global INITIAL_ACCELERATION
+    global SPEED
+    global ACCELERATION
     noise = OpenSimplex()
     # Increasing this value would mean moving faster accross perlin noise space, meaning the difference between each value would be greater.
     NOISE_COUNTER_INCREMENT = 0.1
@@ -41,30 +47,55 @@ def simulate_speed():
         elif ACCELERATION > 5:
             ACCELERATION = 5
 
+        noise_counter += NOISE_COUNTER_INCREMENT
+        # Only do this loop once a second..
+        time.sleep(1)
+
+
+# Create the application instance
+app = flask.Flask(__name__, template_folder="templates")
+SENSOR_TYPE = ""
+
+
+@app.route('/')
+def getSpeed():
+    keys = []
+    values = []
+    if SENSOR_TYPE == "speed":
+        global INITIAL_SPEED
+        global INITIAL_ACCELERATION
+        global SPEED
+        global ACCELERATION
         keys = ["initialSpeed", "initialAcceleration",
                 "speed", "acceleration", "speedPretty", "accelerationPretty"]
         values = [INITIAL_SPEED, INITIAL_ACCELERATION, SPEED, ACCELERATION, str(
             SPEED) + " km/h", str(ACCELERATION) + " km/h"]
 
-        # I guess we make this packet available to gram from some port?
-        myPacket = construct_packet(keys, values)
-        print(myPacket)
-        print("")
-
-        noise_counter += NOISE_COUNTER_INCREMENT
-        # Only do this loop once a second..
-        time.sleep(1)
+    # I guess we make this packet available to gram from some port?
+    myPacket = construct_packet(keys, values)
+    return myPacket
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Simulates some sensors')
     parser.add_argument('--sensor-type', metavar='sensor-type', type=str,
                         help='The type of sensor to simulate')
+    parser.add_argument('--port', metavar='port', type=int,
+                        help='The port for the sensor output to be broadcasted to')
     args = parser.parse_args()
 
     if args.sensor_type is None:
         print("Please specify the type of sensor to simulate")
         exit(1)
 
+    SENSOR_TYPE = args.sensor_type
+
     if args.sensor_type == "speed":
-        simulate_speed()
+        x = threading.Thread(target=simulate_speed)
+        x.daemon = True
+        x.start()
+
+    myPort = 5000
+    if args.port:
+        myPort = args.port
+    app.run(debug=True, port=myPort)
