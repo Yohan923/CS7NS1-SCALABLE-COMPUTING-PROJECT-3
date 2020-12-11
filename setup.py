1516bcf7081f1d42b513c59c6312b68cf91cac1b
john <johnz0923@gmail.com>
2020-12-11 17:28:46 +0000

    squash! misc

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from os import path, mkdir, system
 
 # the commands are setup specific to linux
 COMMANDS = [
-    '. ./venv/bin/activate',
     'pip install awsiotsdk',
     'pip install boto3'
 ]
@@ -34,54 +33,58 @@ def check_verbosity(verbosity):
 
 
 def setup_aws():
-    from src.iot_core.utils import create_a_thing, get_endpoint, describe_thing
     
     if not path.exists('~/.aws'):
         mkdir('~/.aws')
 
+    with open('~/.aws/credentials', 'w') as cred:
+        with open('~/.aws/config', 'w') as conf:
+            aws_key = get_input('enter aws access key id: ')
+            aws_secret = get_input('enter aws secret access key: ')
+            
+            cred.writelines([
+                f'[default]',
+                f'aws_access_key_id = {aws_key}',
+                f'aws_secret_access_key = {aws_secret}'
+            ])
+
+            conf.writelines([
+                f'[default]',
+                f'region = eu-west-1'
+            ])
+            
+            cred.close()
+            conf.close()
+
+    # configure your thing
+    from src.iot_core.utils import create_a_thing, get_endpoint, describe_thing
     with open('./src/consts/iot_core.py', 'w') as f:
-        with open('~/.aws/credentials') as cred:
-            with open('~/.aws/config') as conf:
-                thing_name = get_input('enter a name for your thing, e.g. username-device: ')
-                thing_type = get_input('enter a type for your thing or leave it blank: ', default='')
-                aws_key = get_input('enter aws access key id: ')
-                aws_secret = get_input('enter aws secret access key: ')
-                root_cert_path = '../certs/root-CA.crt'
-                client_id = get_input('enter client_id, press enter for default[pie]: ', default='pie')
-                signing_region = 'eu-west-1'
-                proxy_host = get_input('enter host address of proxy, press enter for default[www-proxy.scss.tcd.ie]: ', default='www-proxy.scss.tcd.ie')
-                proxy_port = get_input('enter port of proxy, press enter for default[8080]: ', default='8080')
-                verbosity = get_input('enter log verbosity, choices = {NoLogs,Fatal,Error,Warn,Info,Debug,Trace}, default[NoLogs]: ', check_verbosity, 'NoLogs')
-                
-                thing_arn = create_a_thing(thing_name, thing_type)
-                while not thing_arn:
-                    thing_arn = describe_thing(thing_name)
+        thing_name = get_input('enter a name for your thing, e.g. username-device: ')
+        thing_type = get_input('enter a type for your thing or leave it blank: ', default='')
+        root_cert_path = '../certs/root-CA.crt'
+        client_id = get_input('enter client_id, press enter for default[pie]: ', default='pie')
+        signing_region = 'eu-west-1'
+        proxy_host = get_input('enter host address of proxy, press enter for default[www-proxy.scss.tcd.ie]: ', default='www-proxy.scss.tcd.ie')
+        proxy_port = get_input('enter port of proxy, press enter for default[8080]: ', default='8080')
+        verbosity = get_input('enter log verbosity, choices = {NoLogs,Fatal,Error,Warn,Info,Debug,Trace}, default[NoLogs]: ', check_verbosity, 'NoLogs')
+        
+        thing_arn = create_a_thing(thing_name, thing_type)
+        while not thing_arn:
+            thing_arn = describe_thing(thing_name)
 
-                f.writelines([
-                    f'ENDPOINT=\'{get_endpoint()}\'\n',
-                    f'THING_ARN=\'{thing_arn}\'\n'
-                    f'ROOT_CERT_PATH=\'{root_cert_path}\'\n',
-                    f'CLIENT_ID=\'{client_id}\'\n',
-                    f'SIGNING_REGION=\'{signing_region}\'\n',
-                    f'PROXY_HOST=\'{proxy_host}\'\n',
-                    f'PROXY_PORT={proxy_port}\n',
-                    f'VERBOSITY=\'{verbosity}\'\n'
-                ])
+        f.writelines([
+            f'ENDPOINT=\'{get_endpoint()}\'\n',
+            f'THING_ARN=\'{thing_arn}\'\n'
+            f'ROOT_CERT_PATH=\'{root_cert_path}\'\n',
+            f'CLIENT_ID=\'{client_id}\'\n',
+            f'SIGNING_REGION=\'{signing_region}\'\n',
+            f'PROXY_HOST=\'{proxy_host}\'\n',
+            f'PROXY_PORT={proxy_port}\n',
+            f'VERBOSITY=\'{verbosity}\'\n'
+        ])
 
-                cred.writelines([
-                    f'[default]',
-                    f'aws_access_key_id = {aws_key}',
-                    f'aws_secret_access_key = {aws_secret}'
-                ])
+        f.close()
 
-                conf.writelines([
-                    f'[default]',
-                    f'region = eu-west-1'
-                ])
-
-                f.close()
-                cred.close()
-                conf.close()
         
 
 if __name__ == "__main__":
@@ -92,8 +95,10 @@ if __name__ == "__main__":
     if not path.exists('./certs/root-CA.crt'):
         mkdir('./certs')
         system('curl https://www.amazontrust.com/repository/AmazonRootCA1.pem > ./certs/root-CA.crt')
-
-    # activate venv and install dependencies
+    
+    # activate venv 
+    system('. ./venv/bin/activate')
+    # install dependencies
     system(' && '.join(COMMANDS))
 
     setup_aws()
