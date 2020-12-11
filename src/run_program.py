7ac7836aa9262092132c7f10b6a0131b3fa84648
john <johnz0923@gmail.com>
2020-12-11 18:47:14 +0000

    connection done

--- src/run_program.py
+++ src/run_program.py
@@ -1,7 +1,18 @@
 # this should the entry point of everything
-
-from iot_core.connections import connect_to_thing_via_mqtt_with_websock
+import time
+from iot_core.connections import MQTTConnection
 
 if __name__ == "__main__":
 
-    connect_to_thing_via_mqtt_with_websock()
+    def test_callback(topic, payload, **kwargs):
+        print("Received message from topic '{}': {}".format(topic, payload))
+
+    new_connection = MQTTConnection()
+
+    new_connection.connect()
+
+    new_connection.subscribe('test-topic', callback=test_callback)
+
+    for i in range(10):
+        new_connection.publish('test-topic', f'wtf {i}')
+        time.sleep(1)
