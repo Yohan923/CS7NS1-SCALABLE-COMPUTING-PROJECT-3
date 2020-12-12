97aedbf95bc9871d262ad3c6625aa657710f5b48
caop <820672172@qq.com>
2020-12-12 22:07:05 +0000

    Update __init__.py

--- src/node/devices/__init__.py
+++ src/node/devices/__init__.py
@@ -1,10 +1,10 @@
-import headway_sensor
-from headway_sensor import *
-from light_controller import *
-from photo_sensor import *
-from rainfall_sensor import *
-from speed_sensor import *
-from wiper_controller import *
-from mqtt_client import *
-from communication_device import *
-from listener import *
+# import headway_sensor
+# from headway_sensor import *
+# from light_controller import *
+# from photo_sensor import *
+# from rainfall_sensor import *
+# from speed_sensor import *
+# from wiper_controller import *
+# from mqtt_client import *
+# from communication_device import *
+# from listener import *
