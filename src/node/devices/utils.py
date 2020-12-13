e9c879f085e55330177d9b8cafffef6469b28f39
caop <820672172@qq.com>
2020-12-13 14:57:11 +0000

    k

--- src/node/devices/utils.py
+++ src/node/devices/utils.py
@@ -3,21 +3,5 @@ import json
 
 
 def aggregate_full_vehicle_states():
-<<<<<<< HEAD
-
     return my_vehicle.aggregate_full_vehicle_states();
-=======
-    # payload = {
-    #     'x': str(my_vehicle.x),
-    #     'y': str(my_vehicle.y),
-    #     'speed': str(my_vehicle.speed_sensor.get_speed())
-    #     # add more later
-    # }
-    payload = {
-        'x': str(config.my_vehicle.x),
-        'y': str(config.my_vehicle.y),
-        'speed': str(config.my_vehicle.speed_sensor.get_speed())
-        # add more later
-    }
-    return json.dumps(payload)
->>>>>>> 70c15cb1170d526fe923bffa7a6c2e80721e45b5
+
