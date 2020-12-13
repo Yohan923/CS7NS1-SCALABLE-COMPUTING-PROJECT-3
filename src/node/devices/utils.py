371bc6c69f9bd12822f524a0e039e8d40aac5f4c
john <johnz0923@gmail.com>
2020-12-13 14:33:13 +0000

    backbone server code

--- src/node/devices/utils.py
+++ src/node/devices/utils.py
@@ -1,12 +1,12 @@
-from config import my_vehicle
+import config
 import json
 
 
 def aggregate_full_vehicle_states():
     payload = {
-        'x': str(my_vehicle.x),
-        'y': str(my_vehicle.y),
-        'speed': str(my_vehicle.speed_sensor.get_speed())
+        'x': str(config.my_vehicle.x),
+        'y': str(config.my_vehicle.y),
+        'speed': str(config.my_vehicle.speed_sensor.get_speed())
         # add more later
     }
 
