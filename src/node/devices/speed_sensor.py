2c7925e711edf0ddf2852e7931e893702488015e
caop <820672172@qq.com>
2020-12-14 15:42:05 +0000

    Update speed_sensor.py

--- src/node/devices/speed_sensor.py
+++ src/node/devices/speed_sensor.py
@@ -54,9 +54,6 @@ class SpeedSensor(threading.Thread):
         self.visualizer=Visualizer(self.neighbours,clear=True,table=True, road_map=True)
         self.visualizer.update_car_list(nid,self.constrct_dict())
 
-        # self.track = Track()
-        # self.myself = Car( Stats( (self.LOC,self.LANE), self.SPEED, self.ACCELERATION ), track=self.track)
-        # self.track.Add(self.myself)
         self.update_timer = 0
         self.draw_timer = 0     
 
@@ -99,12 +96,13 @@ class SpeedSensor(threading.Thread):
 
         try:
             myPacket = self.construct_packet(keys, values)
+            print(myPacket)
             myPacket_bytes = bytes(myPacket, 'utf-8')
             self.send(myPacket_bytes)   
 
             # Restart the timer
             self.update_timer.cancel()
-            self.update_timer = Timer(UPDATE_INTERVAL, self.update(), ())
+            self.update_timer = Timer(UPDATE_INTERVAL, self.update, ())
             self.update_timer.start() 
             print("TODO")
         except:
@@ -175,10 +173,10 @@ class SpeedSensor(threading.Thread):
         self.sock.setblocking(0)
 
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.update_timer = Timer(UPDATE_INTERVAL, self.update(), ())
-        self.draw_timer = Timer(DRAW_INTERVAL,  self.visualizer.run(self.draw_timer), ())        
+        self.update_timer = Timer(UPDATE_INTERVAL, self.update, ())
+        # self.draw_timer = Timer(DRAW_INTERVAL,  self.visualizer.run(self.draw_timer), ())        
         self.update_timer.start()
-        self.draw_timer.start()
+        # self.draw_timer.start()
 
         while (True):
             # receive
@@ -201,160 +199,6 @@ class SpeedSensor(threading.Thread):
 
 
 
-
-
-
-
-class Stats:
-    
-    def __init__(self, location=(0,0), speed=0, acceleration=0):
-        self.location = location
-        self.speed = speed
-        self.acceleration = acceleration
-
-        
-    def __str__(self):
-        _str = "[{}  {}  {}]".format(self.location, self.speed, self.acceleration)
-        return _str
-    
-    
-class Track:
-    
-    def __init__(self, length = TOTAL_LENGHT):
-        
-        print("Initializing track\n")
-        self.track = np.zeros((length, 2))
-        self.cars = {}
-        self.i_cars = 1
-        self.is_running = True
-        
-    def __str__(self):
-        spots_occupied = np.argwhere(self.track!=0)[:, 0].ravel()
-        _str = "[TRACK {}]  ".format(spots_occupied)
-        for car in self.cars.keys():
-            _str = _str + "{}: {} , ".format(self.cars[car], car.stats)
-        return _str
-        
-    def Add(self, car):
-        self.cars[car] = self.i_cars 
-        self.track[car.stats.location[0], car.stats.location[1]] = self.i_cars
-        self.i_cars = self.i_cars + 1
-        
-    def Stop(self):
-        print("\nDestroying track")
-        self.is_running = False
-        for car in self.cars.keys():
-            car.Stop()
-            
-    def GetNieghbbours(self, car, area=30):
-        car_id = self.cars[car]
-        car_loc = car.stats.location[0]
-        nieghbourhood = self.track[ (car_loc-area):(car_loc+area)]
-        ids = nieghbourhood[nieghbourhood!=0].ravel()
-        cars = [key for key in self.cars if (self.cars[key] in ids) and self.cars[key]!=car_id ] 
-        cars = np.array(cars)
-        distances = [abs(car.stats.location[0] - other.stats.location[0]) for other in cars]
-        cars = cars[np.argsort(distances)]
-        
-        return cars
-        
-             
-    def UpdateLocations(self):
-        i=0
-        while(self.is_running):
-            cars = list(self.cars.keys())
-            for car in cars:
-                car_id = self.cars[car]
-                try:
-                    prev_loc = np.argwhere(self.track==car_id).ravel()
-                    self.track[prev_loc[0], prev_loc[1]] = 0
-                except:
-                    pass
-                
-                if(car.is_running):
-                    
-                    if(self.track[car.stats.location[0], car.stats.location[1]]==0):
-                        self.track[car.stats.location[0], car.stats.location[1]] = car_id
-                else:
-                    del self.cars[car]
-                    
-            i=i+1
-            if(i%10==0):
-                print(self)
-            time.sleep(0.1)
-            
-class Car:
-    
-    def __init__(self, stats, track, passive=True):
-        self.stats = stats
-        self.neighbours = []
-        self.track = track
-        self.track.Add(self)
-        self.is_running = True
-        if(not passive):
-            Thread(target=self.Monitor).start()
-            Thread(target=self.CalulatePositionAndSpeed).start()
-            
-    def __str__(self):  
-        _str = "<Car {}>".format(self.stats.location)
-        return _str
-    
-    
-    def Stop(self):
-        self.is_running = False
-        
-        
-    def Update(self, location, speed, acceleration):
-        self.stats.location = location
-        self.stats.speed = speed
-        self.stats.acceleration = acceleration
-        
-
-    def CalulatePositionAndSpeed(self, step=1):  
-        while(self.is_running):
-            
-            prev_loc = self.stats.location
-            new_loc = ( int( prev_loc[0] + (self.stats.speed*step) )%self.track.track.shape[0] , prev_loc[1] )
-            self.stats.location = new_loc
-            self.stats.speed = self.stats.speed + (self.stats.acceleration*step)
-            self.stats.speed = max (0, self.stats.speed)
-            time.sleep(step)
-      
-        
-    def Monitor(self):
-        while(self.is_running):
-            
-            self.neighbours = self.track.GetNieghbbours(self)
-            self.ControlSpeedAndAcceleration()
-            
-            #print(self.stats)
-            time.sleep(1)
-            
-    
-    def SwitchLanes(self):
-        self.stats.location = ( self.stats.location[0], 1-self.stats.location[1] )
-            
-    
-    def ControlSpeedAndAcceleration(self):
-        #print([str(x) for x in self.neighbours])
-        
-
-        if(len(self.neighbours) > 0):
-            closest_car = self.neighbours[0]
-            self.stats.location = (self.stats.location[0], (1-closest_car.stats.location[1]) )
-            if(self.stats.location[0] < closest_car.stats.location[0]):
-                #print("Acceleration")
-                self.stats.acceleration = MAX_ACCELERATION
-            else:
-                #print("Decceleration")
-                self.stats.acceleration = MIN_ACCELERATION
-        
-        elif(self.stats.speed < (MAX_SPEED)):
-            self.stats.acceleration = MAX_ACCELERATION
-        
-        elif(self.stats.speed > (MAX_SPEED)):
-            self.stats.acceleration = MIN_ACCELERATION
-               
 
 #####################################################
 #####################################################
