205ede809ed7790b8eca5e9e7c98a7ecfc105a36
caop <820672172@qq.com>
2020-12-14 15:08:07 +0000

    Update speed_sensor.py

--- src/node/devices/speed_sensor.py
+++ src/node/devices/speed_sensor.py
@@ -176,7 +176,7 @@ class SpeedSensor(threading.Thread):
 
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.update_timer = Timer(UPDATE_INTERVAL, self.update(), ())
-        self.draw_timer = Timer(DRAW_INTERVAL,  self.visualizer.run(), draw_timer)        
+        self.draw_timer = Timer(DRAW_INTERVAL,  self.visualizer.run(draw_timer), ())        
         self.update_timer.start()
         self.draw_timer.start()
 
@@ -395,7 +395,7 @@ class Visualizer:
             self.GenerateTable()
         try:
             draw_timer.cancel()
-            draw_timer = Timer(DRAW_INTERVAL, self.run(), draw_timer)
+            draw_timer = Timer(DRAW_INTERVAL, self.run(draw_timer), draw_timer)
             draw_timer.start() 
             print("TODO 2")
         except:
