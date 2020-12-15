5f023545fdf876cafd531b9698929d9262a12a60
Cian Nugent McGealy <cnugentmcgealy@gmail.com>
2020-12-15 23:00:18 +0000

    updated mqtt topic in intermediary

--- src/node/devices/intermediary_mqtt_client.py
+++ src/node/devices/intermediary_mqtt_client.py
@@ -6,18 +6,7 @@ class IntermediateMQTTClient(MQTTClient):
         
         # TODO: process received message and send to group 9
 
-    def subscribe(self, topic, qos=mqtt.QoS.AT_LEAST_ONCE, callback=None):
-        print("Subscribing to topic '{}'...".format(topic))
-        subscribe_future, packet_id = self._connection.subscribe(
-            topic=topic,
-            qos=qos,
-            callback=callback)
-
-        subscribe_result = subscribe_future.result()
-        print("Subscribed with {}".format(str(subscribe_result['qos'])))
-        return packet_id
-    
     def run(self):
         self.connect()
 
-        self.subscribe("group9", callback=on_message_received)
+        self.subscribe("vehicles", callback=on_message_received)
