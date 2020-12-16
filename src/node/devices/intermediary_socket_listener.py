6d47de5e02a6928975f5c0455869aeaf71f1366a
Cian Nugent McGealy <cnugentmcgealy@gmail.com>
2020-12-16 12:40:44 +0000

    Mostly complete communication with other group

--- src/node/devices/intermediary_socket_listener.py
+++ src/node/devices/intermediary_socket_listener.py
@@ -12,19 +12,24 @@ class IntermediarySocketListener(Thread):
         self.remote_listen_port = remote_listen_port
 
     def run(self):
-        listener_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        listener_sock.bind(('localhost', self.remote_listen_port))
-        listener_sock.setblocking(0)
-        listener_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listener_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener_sock.bind(('0.0.0.0', self.remote_listen_port))
+        self.listener_sock.listen(10)
+        self.listener_sock.setblocking(0)
+        self.listener_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        inputs = [listener_sock]
+        inputs = [self.listener_sock]
         outputs = []
 
-        print("running listener")
 
         while inputs:
             readable, _, _ = select.select(inputs, outputs, inputs)
             for r in readable:
-                if r is listener_sock:
-                    pass
-                    # TODO process group 9's json payload and upload to mqtt
+                if r is self.listener_sock:
+                    connection, client_address = r.accept()
+                    message, _ = connection.recvfrom(200)
+
+                    # Process message and push to mqtt
+                    # Example:
+                    #   b'{"id": 3, "speed": 69, "brightness": 54662, "fuel": 51.05269686379842}'
+                    print(message)
