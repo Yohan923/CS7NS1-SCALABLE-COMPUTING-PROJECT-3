fb9ad23398d511dedf4677b6a9e2e51259c2a58e
caop <820672172@qq.com>
2020-12-16 13:00:50 +0000

    t

--- src/node/devices/light_controller.py
+++ src/node/devices/light_controller.py
@@ -70,7 +70,9 @@ class LightController(Thread):
             try:
                 message, _ = self.sock.recvfrom(100)
                 message = message.decode('utf-8')
+                print(message)
                 message=json.loads(message)
+                print(message)
                 if 'light_intensity' in message.keys():
                     self.set_speed_by_photo_intensity(message['light_intensity'])
 
